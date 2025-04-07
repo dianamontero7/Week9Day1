54f1d78ead56775cba8e8deb191b44b3887ff3dd
dianamontero7 <dianamontero38@gmail.com>
2025-04-07 18:30:59 -0500

    initial commit

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -2,18 +2,27 @@
 
 function App() {
   return (
-    <div className="App" style={{ border: "6px solid black" }}>
+    <div className="App"  style={{ display: "flex", flexDirection: "column", height: "100vh", }}>
 
       {/* Part 1: Navigation Bar */}
-      <div id="NavigationBar" style={{ border: "6px solid #FF0000", height: "15vh", textAlign: "center" }}>
+      <div id="NavigationBar" style={{ border: "6px solid #FF0000",  textAlign: "center", flex: "0 0 15%", display: "flex", justifyContent: "center"}}>
         <h1>Navigation Bar</h1>
       </div>
 
       {/* Part 2: Content Section */}
+      <div id="ContentSection" style={{border: "6px solid #39FF14", textAlign: "center", flex: 1, display: "flex", justifyContent: "center", flexDirection: "column"}}>
+        <h1>Content</h1>
 
+        <div id="MainContentSection" style={{border: "6px solid #BF40BF", textAlign: "center", flex: 1, margin: "0px 60px", display: "flex", justifyContent: "center", margin: "30px 30px"}}>
+        <h1>Main Content</h1>
+        </div>
+      </div>
 
       {/* Part 3: Footer Section */}
 
+      <div id="FooterSection" style={{border: "6px solid #1F51FF", textAlign: "center", flex: "0 0 15%", display: "flex", justifyContent: "center"}}>
+        <h1>Footer</h1>
+      </div>
 
 
 
